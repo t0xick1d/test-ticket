3e8ac66ba4dbb278c939aae9c15eaba1db0ad3d4
t0xick1d <flaider1999@gmail.com>
2025-06-17 16:30:37 +0300

    added format price

--- src/pages/GridTickets/GridTickets.tsx
+++ src/pages/GridTickets/GridTickets.tsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Spinner } from '../../components';
 import { useGetTicketsQuery, useGetSearchIdQuery } from '../../redux/ducks/tickets';
 import Paper from '@mui/material/Paper';
-import { Column } from '@devexpress/dx-react-grid';
+import { Column, DataTypeProvider, DataTypeProviderProps } from '@devexpress/dx-react-grid';
 import { Grid, Table, TableHeaderRow, TableRowDetail } from '@devexpress/dx-react-grid-material-ui';
 import { RowDetailState, SortingState, IntegratedSorting } from '@devexpress/dx-react-grid';
 import Item from './Item';
 
 import style from './style.module.scss';
 import { TicketsI } from 'types/TicketsInterface';
+import { JSX } from 'react/jsx-runtime';
 
 const TicketsSearch: React.FC = () => {
    const [columns] = useState<Column[]>([
@@ -22,11 +23,18 @@ const TicketsSearch: React.FC = () => {
    const searchid = useGetSearchIdQuery('');
    const ticket = useGetTicketsQuery('sdasdasdsa');
    const [rows, setRows] = useState<TicketsI[]>([]);
+   const [currencyColumns] = useState(['price']);
    useEffect(() => {
       if (!ticket.isLoading && ticket.data !== undefined) {
          setRows(ticket.data);
       }
    }, [ticket, ticket.isLoading]);
+
+   const PriceFormatter = ({ value }: any) => <div>{value} P</div>;
+
+   const CurrencyTypeProvider = (props: JSX.IntrinsicAttributes & DataTypeProviderProps) => (
+      <DataTypeProvider formatterComponent={PriceFormatter} {...props} />
+   );
 
    if (ticket.isLoading) {
       return <Spinner />;
@@ -44,6 +52,7 @@ const TicketsSearch: React.FC = () => {
                         expandedRowIds={expandedRowIds}
                         onExpandedRowIdsChange={setExpandedRowIds}
                      />
+                     <CurrencyTypeProvider for={currencyColumns} />
                      <SortingState />
                      <IntegratedSorting />
                      <Table />
