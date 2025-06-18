34131ccd5f3acc4e298fe21ea28123bc5621057d
t0xick1d <flaider1999@gmail.com>
2025-06-18 09:51:31 +0300

    fixed chart component

--- src/pages/Chart/Chart.tsx
+++ src/pages/Chart/Chart.tsx
@@ -6,7 +6,7 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Chart = () => {
-   const [dataChart, setDataChart] = useState({
+   const [dataChart] = useState({
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [
          {
